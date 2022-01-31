8d9565cc338943deff0bec6c50236a5d8a95b6c6
sinus-x <14353790+sinus-x@users.noreply.github.com>
2022-01-31 10:12:11 +0100

    Add support for comparison between content and with other members

--- dump/module.py
+++ dump/module.py
@@ -1,11 +1,11 @@
 import os
-import tempfile
 from pathlib import Path
+from typing import Optional
 
 import nextcord
 from nextcord.ext import commands
 
-from pie import check, exceptions, i18n, logger, utils
+from pie import exceptions, i18n, logger, utils
 
 import grapher
 
@@ -35,12 +35,15 @@ class Dump(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.max_concurrency(1, per=commands.BucketType.default, wait=False)
     @commands.group(name="db-dump")
     async def dbdump_(self, ctx):
+        """Display long-term bot usage statistics."""
         await utils.discord.send_help(ctx)
 
     @dbdump_.command(name="options")
     async def dbdump_options(self, ctx):
+        """Display which data are available."""
         await ctx.reply(
             _(
                 ctx,
@@ -53,10 +56,86 @@ class Dump(commands.Cog):
             + ", ".join(grapher.extract.CONTENT)
         )
 
-    @commands.max_concurrency(1, per=commands.BucketType.default, wait=False)
     @commands.cooldown(rate=3, per=60, type=commands.BucketType.user)
     @dbdump_.command(name="get")
-    async def dbdump_get(self, ctx, content: str):
+    async def dbdump_get(
+        self,
+        ctx,
+        content: str,
+        content2: Optional[str] = None,
+        content3: Optional[str] = None,
+    ):
+        """Display your statistics for up to three types of content."""
+        if content not in grapher.extract.CONTENT:
+            await ctx.reply(_(ctx, "I don't know how to show this kind of data."))
+            return
+        if content2 is not None and content2 not in grapher.extract.CONTENT:
+            await ctx.reply(
+                _(ctx, "I don't know how to display the second type of content.")
+            )
+            return
+        if content3 is not None and content3 not in grapher.extract.CONTENT:
+            await ctx.reply(
+                _(ctx, "I don't know how to display the third type of content.")
+            )
+            return
+
+        files = grapher.extract._scan_directory(PUMPKIN_DUMP_DIRECTORY)
+
+        async with ctx.typing():
+            scanner = grapher.extract.scanners.get_scanner(content)
+            data0 = scanner.search(ctx.guild.id, ctx.author.id, files)
+
+            if content2:
+                scanner2 = grapher.extract.scanners.get_scanner(content2)
+                data2 = scanner2.search(ctx.guild.id, ctx.author.id, files)
+            else:
+                data2 = None
+
+            if content3:
+                scanner3 = grapher.extract.scanners.get_scanner(content3)
+                data3 = scanner3.search(ctx.guild.id, ctx.author.id, files)
+            else:
+                data3 = None
+
+            writer = grapher.extract.writers.CSVWriter(scanner, data0)
+
+            Path("/tmp/pumpkin.py/").mkdir(exist_ok=True)
+            csv = Path(f"/tmp/pumpkin.py/{ctx.guild.id}_{ctx.author.id}_{content}.csv")
+            png = Path(f"/tmp/pumpkin.py/{ctx.guild.id}_{ctx.author.id}_{content}.png")
+
+            writer.dump(csv)
+
+            chart = grapher.graph.graph(csv, series_name=content)
+            if data2 is not None:
+                chart.add(content2, [v for v in data2.values()])
+            if data3 is not None:
+                chart.add(content3, [v for v in data3.values()])
+
+            timespan = chart.title
+            chart.title = f"{ctx.author.name}, {ctx.guild.name}\n{timespan}"
+
+            chart.render_to_png(str(png))
+
+            with png.open("rb") as handle:
+                await ctx.reply(
+                    file=nextcord.File(fp=handle, filename=f"{content}.png")
+                )
+
+        csv.unlink()
+        png.unlink()
+
+    @commands.cooldown(rate=3, per=60, type=commands.BucketType.user)
+    @dbdump_.command(name="compare")
+    async def dbdump_compare(
+        self,
+        ctx,
+        content: str,
+        member1: nextcord.Member,
+        member2: Optional[nextcord.Member] = None,
+        member3: Optional[nextcord.Member] = None,
+    ):
+        """Compare your statistics with up to three other members."""
         if content not in grapher.extract.CONTENT:
             await ctx.reply(_(ctx, "I don't know how to show this kind of data."))
             return
@@ -65,20 +144,28 @@ class Dump(commands.Cog):
         files = grapher.extract._scan_directory(PUMPKIN_DUMP_DIRECTORY)
 
         async with ctx.typing():
-            data = scanner.search(ctx.guild.id, ctx.author.id, files)
+            data0 = scanner.search(ctx.guild.id, ctx.author.id, files)
+            data1 = scanner.search(ctx.guild.id, member1.id, files)
+            data2 = scanner.search(ctx.guild.id, member2.id, files) if member2 else None
+            data3 = scanner.search(ctx.guild.id, member3.id, files) if member3 else None
 
-            writer = grapher.extract.writers.CSVWriter(scanner, data)
+            writer = grapher.extract.writers.CSVWriter(scanner, data0)
 
             Path("/tmp/pumpkin.py/").mkdir(exist_ok=True)
             csv = Path(f"/tmp/pumpkin.py/{ctx.guild.id}_{ctx.author.id}_{content}.csv")
-            png = Path("/tmp/pumpkin.py/dump.png")
+            png = Path(f"/tmp/pumpkin.py/{ctx.guild.id}_{ctx.author.id}_{content}.png")
 
             writer.dump(csv)
+            chart = grapher.graph.graph(csv, series_name=ctx.author.name)
 
-            chart = grapher.graph.graph(csv, content)
+            chart.add(member1.display_name, [v for v in data1.values()])
+            if data2 is not None:
+                chart.add(member2.display_name, [v for v in data2.values()])
+            if data3 is not None:
+                chart.add(member3.display_name, [v for v in data3.values()])
 
             timespan = chart.title
-            chart.title = f"{content}, {ctx.author.name}, {ctx.guild.name}\n{timespan}"
+            chart.title = f"{content}, {ctx.guild.name}\n{timespan}"
 
             chart.render_to_png(str(png))
 
